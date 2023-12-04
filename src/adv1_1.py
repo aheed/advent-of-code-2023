6d61191da8b8f4e1d8f1255085d7ed58e91e2e53
Anders Heed <aheed88888@gmail.com>
2023-12-05 00:09:16 +0100

    1_1 done

--- src/adv1_1.py
+++ src/adv1_1.py
@@ -3,4 +3,7 @@ import utils
 with utils.get_in_file() as infile:
     lines = [line.strip() for line in infile]
 
-print(lines)
+def get_first_digit(line: str) -> str:
+    return next((c for c in line if c.isnumeric()))
+
+print(sum([int(get_first_digit(line) + get_first_digit(line[::-1])) for line in lines]))
