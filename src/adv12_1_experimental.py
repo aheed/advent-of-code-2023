7ae66caa9f6036f240fb9d707ea9cf6ae4d33b95
Anders Heed <aheed88888@gmail.com>
2023-12-14 13:06:05 +0100

    corrected adv12_1_experimental.py

--- src/adv12_1_experimental.py
+++ src/adv12_1_experimental.py
@@ -36,7 +36,7 @@ def calc_nof_arrangements(row: list[Status], groups: list[int]) -> int:
     #    return 0
     
     if len(groups) == 0:
-        return 1
+        return 0 if Status.YES in row else 1
 
     #if len(groups) == 0:
     #    if len(row) == 0:
@@ -44,7 +44,10 @@ def calc_nof_arrangements(row: list[Status], groups: list[int]) -> int:
     #    return 0
     last_possible_start_pos = row.index(Status.YES)+1 if Status.YES in row else len(row)
     group0_positions = [i for i in range(last_possible_start_pos) if group_fits_at(row=row, pos=i, group=groups[0])]
-    return sum((calc_nof_arrangements(row=row[i + groups[0]+1:], groups=groups[1:]) for i in group0_positions))
+    #print(group0_positions)
+    arrs = [calc_nof_arrangements(row=row[i + groups[0]+1:], groups=groups[1:]) for i in group0_positions]
+    ret = sum(arrs)
+    return ret
 
 def char_to_status(c: str) -> Status:
     match c:
@@ -64,14 +67,21 @@ def line_to_row_and_groups(line: str) -> tuple[list[Status], list[int]]:
     return (row, groups)
 
 rows_and_groups = [line_to_row_and_groups(line=line) for line in lines]
-print(rows_and_groups[0])
 
-#print(calc_nof_arrangements(row=rows_and_groups[-1][0], groups=rows_and_groups[-1][1]))
-#print([calc_nof_arrangements(row=r_n_g[0], groups=r_n_g[1]) for r_n_g in rows_and_groups])
-#print(sum([calc_nof_arrangements(row=r_n_g[0], groups=r_n_g[1]) for r_n_g in rows_and_groups]))
+#print(rows_and_groups[1])
+#print(calc_nof_arrangements(row=rows_and_groups[1][0], groups=rows_and_groups[1][1]))
 
-test = line_to_row_and_groups("???? 2,1")
-print(calc_nof_arrangements(row=test[0], groups=test[1]))
+#a = [calc_nof_arrangements(row=r_n_g[0], groups=r_n_g[1]) for r_n_g in rows_and_groups]
+#print(a)
+#for arr in a:
+#    print(arr)
+    
+#print("sum:", sum(a))
+print([calc_nof_arrangements(row=r_n_g[0], groups=r_n_g[1]) for r_n_g in rows_and_groups])
+print(sum([calc_nof_arrangements(row=r_n_g[0], groups=r_n_g[1]) for r_n_g in rows_and_groups]))
+
+#test = line_to_row_and_groups("???? 2,1")
+#print(calc_nof_arrangements(row=test[0], groups=test[1]))
 
 # 9241 is too high
 # 6434 is too low
