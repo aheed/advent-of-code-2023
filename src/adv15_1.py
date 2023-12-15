172d592f486214e2a83fe2128553c4fa23f77e09
Anders Heed <aheed88888@gmail.com>
2023-12-15 23:31:51 +0100

    15_1 done

---
+++ src/adv15_1.py
@@ -0,0 +1,16 @@
+import utils
+
+with utils.get_in_file() as infile:
+    lines = [line.strip() for line in infile]
+
+steps = lines[0].split(",")
+print(steps[0], steps[-1])
+
+def hash(in_str: str) -> int:
+    h = 0
+    for c in in_str:
+        h = ((h + ord(c)) * 17) % 256
+    return h
+
+hashes = [hash(in_str=step) for step in steps]
+print(sum(hashes))
