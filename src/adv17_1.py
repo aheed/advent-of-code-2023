7ef8a69b7107ec6ca0c23973efd83dd489292ae7
Anders Heed <aheed88888@gmail.com>
2023-12-17 17:35:30 +0100

    17_1 wip

--- src/adv17_1.py
+++ src/adv17_1.py
@@ -1,3 +1,4 @@
+import sys
 from dataclasses import dataclass
 import utils
 
@@ -6,7 +7,7 @@ class SearchInput:
     direction: str
     x: int
     y:int
-    heat_loss: int
+    #heat_loss: int
     moves_in_same_direction: int
 
 with utils.get_in_file() as infile:
@@ -35,49 +36,56 @@ def traverse_grid(search_input: SearchInput) -> int:
     key = f"{search_input.x},{search_input.y}:{search_input.direction}z{search_input.moves_in_same_direction}"
     if key in cache:
         return cache[key]
+        #pass #temp
     
     if visited[search_input.y][search_input.x]:
         return bad_heat_loss
     visited[search_input.y][search_input.x] = True    
 
-    min_heat_loss = search_input.heat_loss + grid[search_input.y][search_input.x]
+    min_heat_loss = grid[search_input.y][search_input.x] #fixme: should not count for start city block
     if not (search_input.x == len(grid[0])-1 and search_input.y == len(grid)-1):
         new_searches: list[SearchInput] = []
         match search_input.direction:
             case "e":
                 new_searches = [
-                    SearchInput(direction="e", x=search_input.x+1, y=search_input.y, heat_loss=min_heat_loss, moves_in_same_direction=search_input.moves_in_same_direction+1),
-                    SearchInput(direction="n", x=search_input.x, y=search_input.y-1, heat_loss=min_heat_loss, moves_in_same_direction=1),
-                    SearchInput(direction="s", x=search_input.x, y=search_input.y+1, heat_loss=min_heat_loss, moves_in_same_direction=1)
+                    SearchInput(direction="e", x=search_input.x+1, y=search_input.y, moves_in_same_direction=search_input.moves_in_same_direction+1),
+                    SearchInput(direction="n", x=search_input.x, y=search_input.y-1, moves_in_same_direction=1),
+                    SearchInput(direction="s", x=search_input.x, y=search_input.y+1, moves_in_same_direction=1)
                 ]
             case "w":
                 new_searches = [
-                    SearchInput(direction="w", x=search_input.x-1, y=search_input.y, heat_loss=min_heat_loss, moves_in_same_direction=search_input.moves_in_same_direction+1),
-                    SearchInput(direction="n", x=search_input.x, y=search_input.y-1, heat_loss=min_heat_loss, moves_in_same_direction=1),
-                    SearchInput(direction="s", x=search_input.x, y=search_input.y+1, heat_loss=min_heat_loss, moves_in_same_direction=1)
+                    SearchInput(direction="w", x=search_input.x-1, y=search_input.y, moves_in_same_direction=search_input.moves_in_same_direction+1),
+                    SearchInput(direction="n", x=search_input.x, y=search_input.y-1, moves_in_same_direction=1),
+                    SearchInput(direction="s", x=search_input.x, y=search_input.y+1, moves_in_same_direction=1)
                 ]
             case "n":
                 new_searches = [
-                    SearchInput(direction="n", x=search_input.x, y=search_input.y-1, heat_loss=min_heat_loss, moves_in_same_direction=search_input.moves_in_same_direction+1),
-                    SearchInput(direction="e", x=search_input.x+1, y=search_input.y, heat_loss=min_heat_loss, moves_in_same_direction=1),
-                    SearchInput(direction="w", x=search_input.x-1, y=search_input.y, heat_loss=min_heat_loss, moves_in_same_direction=1)
+                    SearchInput(direction="n", x=search_input.x, y=search_input.y-1, moves_in_same_direction=search_input.moves_in_same_direction+1),
+                    SearchInput(direction="e", x=search_input.x+1, y=search_input.y, moves_in_same_direction=1),
+                    SearchInput(direction="w", x=search_input.x-1, y=search_input.y, moves_in_same_direction=1)
                 ]
             case "s":
                 new_searches = [
-                    SearchInput(direction="s", x=search_input.x, y=search_input.y+1, heat_loss=min_heat_loss, moves_in_same_direction=search_input.moves_in_same_direction+1),
-                    SearchInput(direction="e", x=search_input.x+1, y=search_input.y, heat_loss=min_heat_loss, moves_in_same_direction=1),
-                    SearchInput(direction="w", x=search_input.x-1, y=search_input.y, heat_loss=min_heat_loss, moves_in_same_direction=1)
+                    SearchInput(direction="s", x=search_input.x, y=search_input.y+1, moves_in_same_direction=search_input.moves_in_same_direction+1),
+                    SearchInput(direction="e", x=search_input.x+1, y=search_input.y, moves_in_same_direction=1),
+                    SearchInput(direction="w", x=search_input.x-1, y=search_input.y, moves_in_same_direction=1)
                 ]
 
             case _:
                 assert(False)
         search_results = [traverse_grid(search_input=s) for s in new_searches]
-        min_heat_loss = min(min_heat_loss, min(search_results))
+        min_heat_loss = min_heat_loss + min(search_results)
+    else:
+        print("got to the finish line")
 
     visited[search_input.y][search_input.x] = False
     cache[key] = min_heat_loss
     return min_heat_loss
 
-res = traverse_grid(search_input=SearchInput(direction="e", x=0, y=0, heat_loss=0, moves_in_same_direction=0))
+sys.setrecursionlimit(25000)
+res = traverse_grid(search_input=SearchInput(direction="e", x=0, y=0, moves_in_same_direction=0))
+res = res - grid[0][0]
 print(res)
+
+# 771 is too high
 
