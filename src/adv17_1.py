e95e5ba10ac2ef6bb90ea906ccef685d9fcccee9
Anders Heed <aheed88888@gmail.com>
2023-12-19 01:53:06 +0100

    17_1 wip

--- src/adv17_1.py
+++ src/adv17_1.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, replace
 import utils
 
 
@@ -25,6 +25,7 @@ with utils.get_in_file() as infile:
     lines = [line.strip() for line in infile]
 
 grid = [[int(c) for c in line] for line in lines]
+grid[0][0] = 0
 #print(grid)
 
 bad_heat_loss = 1000000000
@@ -59,6 +60,17 @@ def get_neighbors(id: NodeId) -> list[NodeId]:
         case _:
             assert(False)
 
+    #if id.steps_forward < 3:
+    #    ret.append(replace(id, steps_forward=3))
+    #if id.steps_forward < 2:
+    #    ret.append(replace(id, steps_forward=2))
+    for direction in ["n", "s", "e", "w"]:
+        for steps_forward in range(1, 4):
+            #id = NodeId(x=id.x, y=id.y, direction=direction, steps_forward=steps_forward)
+            #ret.append(id)
+            ret.append(replace(id, direction=direction, steps_forward=steps_forward))
+    ret = [n_id for n_id in ret if not (n_id.direction == id.direction and n_id.steps_forward <= id.steps_forward)]
+
     return [n for n in ret if (n.steps_forward <= 3) and not (n.x < 0 or n.x > len(grid[0])-1 or n.y < 0 or n.y > len(grid)-1)]
 
 initial_id = NodeId(x=0, y=0, direction="e", steps_forward=0)
@@ -81,7 +93,8 @@ while True:
         if not neigh_id in nodes:
             nodes[neigh_id] = Node(visited=False, best=bad_heat_loss, heat_loss=grid[neigh_id.y][neigh_id.x])
         if not nodes[neigh_id].visited:
-            nodes[neigh_id].best = min(nodes[neigh_id].best, nodes[neigh_id].heat_loss + nodes[current_id].best)
+            cost = 0 if (current_id.x == neigh_id.x and current_id.y == neigh_id.y) else nodes[neigh_id].heat_loss 
+            nodes[neigh_id].best = min(nodes[neigh_id].best, cost + nodes[current_id].best)
     nodes[current_id].visited = True
 
     lowest_unvisited_best = bad_heat_loss
@@ -89,13 +102,22 @@ while True:
     for id in nodes.keys():
         if (not nodes[id].visited) and nodes[id].best < lowest_unvisited_best:
             lowest_unvisited_id = id
+            lowest_unvisited_best = nodes[id].best
     if lowest_unvisited_id is None:
         break
     current_id = lowest_unvisited_id
-    if current_id == NodeId(x=5, y=1, direction="e", steps_forward=3) or current_id == NodeId(x=5, y=0, direction="n", steps_forward=1):
-        print(nodes[NodeId(x=5, y=1, direction="e", steps_forward=3)]) #20
-        print(nodes[NodeId(x=5, y=0, direction="n", steps_forward=1)]) #should be 23
-        print("zzz")
+    #if current_id == NodeId(x=5, y=1, direction="e", steps_forward=3) or current_id == NodeId(x=5, y=0, direction="n", steps_forward=1):
+    #    print(nodes[NodeId(x=5, y=1, direction="e", steps_forward=3)]) #20
+    #    print(nodes[NodeId(x=5, y=0, direction="n", steps_forward=1)]) #should be 23
+    #    print("zzz")
+
+    dbg = [
+        NodeId(x=2, y=1, direction="s", steps_forward=1), #6
+        NodeId(x=3, y=1, direction="e", steps_forward=1)  #11
+    ]
+    if current_id in dbg:
+        print([nodes[n] for n in dbg])
+        print("...")
 
 
 print(nodes)
@@ -108,11 +130,13 @@ end_nodes_best = [n.best for n in end_nodes]
 print(end_nodes_best)
 print(min(end_nodes_best))
 
-print(nodes[NodeId(x=1, y=0, direction="e", steps_forward=1)])
-print(nodes[NodeId(x=2, y=0, direction="e", steps_forward=2)])
-print(nodes[NodeId(x=2, y=1, direction="s", steps_forward=1)])
-print(nodes[NodeId(x=3, y=1, direction="e", steps_forward=1)])
-print(nodes[NodeId(x=4, y=1, direction="e", steps_forward=2)])
+print(nodes[NodeId(x=1, y=0, direction="e", steps_forward=1)]) #4
+print(nodes[NodeId(x=2, y=0, direction="e", steps_forward=2)]) #5
+print(nodes[NodeId(x=2, y=1, direction="s", steps_forward=1)]) #6
+print(nodes[NodeId(x=3, y=1, direction="e", steps_forward=1)]) #11
+print("?")
+print(nodes[NodeId(x=4, y=1, direction="e", steps_forward=2)]) #should be 15
+print("?")
 print(nodes[NodeId(x=5, y=1, direction="e", steps_forward=3)]) #20
 print("??")
 print(nodes[NodeId(x=5, y=0, direction="n", steps_forward=1)]) #23
