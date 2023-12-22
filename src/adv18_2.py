070142374eb742254f8ce08fdfd8227b16418c12
Anders Heed <aheed88888@gmail.com>
2023-12-22 11:31:41 +0100

    18_2 wip

--- src/adv18_2.py
+++ src/adv18_2.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from dataclasses import dataclass
 import utils
 
@@ -6,6 +7,11 @@ class Instruction:
     direction: str
     distance: int
     dbg: int
+
+class TileStatus(Enum):
+    UNKNOWN = 1
+    OUTSIDE = 3
+    INSIDE = 4
 
 with utils.get_in_file() as infile:
     lines = [line.strip() for line in infile]
@@ -31,34 +37,192 @@ instructions = [get_instruction(line) for line in lines]
 #print(instructions)
 
 
-x=0
-y=0
-min_x = x
-min_y = y
-max_x = x
-max_y = y
+x_coord=0
+y_coord=0
+#min_x = x
+#min_y = y
+#max_x = x
+#max_y = y
+x_coords: list[int] = [0]
+y_coords: list[int] = [0]
 for instr in instructions:
     match instr.direction:
         case "R":
-            x = x + instr.distance
+            x_coord = x_coord + instr.distance
         case "L":
-            x = x - instr.distance
+            x_coord = x_coord - instr.distance
         case "D":
-            y = y + instr.distance
+            y_coord = y_coord + instr.distance
         case "U":
-            y = y - instr.distance
+            y_coord = y_coord - instr.distance
         case _:
             assert(False)
-    min_x = min(x, min_x)
-    max_x = max(x, max_x)
-    min_y = min(y, min_y)
-    max_y = max(y, max_y)
+    x_coords.append(x_coord)
+    y_coords.append(y_coord)
+    #min_x = min(x, min_x)
+    #max_x = max(x, max_x)
+    #min_y = min(y, min_y)
+    #max_y = max(y, max_y)
 
-max_x = max_x - min_x
-max_y = max_y - min_y
+#max_x = max_x - min_x
+#max_y = max_y - min_y
+x_coords = list(set(x_coords)) #remove duplicates
+y_coords = list(set(y_coords)) #remove duplicates
+x_coords.sort()
+y_coords.sort()
 
-print(max_x, max_y)
+print(x_coords, y_coords)
+tiles: list[list[TileStatus]] = [[TileStatus.UNKNOWN for _ in range(len(x_coords))] for _ in range(len(x_coords))]
 
+
+#################
+
+
+#x_coord=0
+#y_coord=0
+x_index = 0
+y_index = 0
+last_direction = "U"
+for instr in instructions:
+    #x_index = x_coords.index(x)
+    #y_index = y_coords.index(y)
+    tiles[y_index][x_index] = TileStatus.INSIDE
+    match instr.direction:
+        case "R":
+            match last_direction:
+                case "U":
+                    if tiles[y_index][x_index-1] == TileStatus.UNKNOWN:
+                        tiles[y_index][x_index-1] = TileStatus.OUTSIDE
+                    if tiles[y_index-1][x_index] == TileStatus.UNKNOWN:
+                        tiles[y_index-1][x_index] = TileStatus.OUTSIDE
+                case "D":
+                    pass
+                case _:
+                    assert(False)
+            target_x_coord = x_coords[x_index] + instr.distance
+            while x_coords[x_index] != target_x_coord:
+                tiles[y_index][x_index] = TileStatus.INSIDE
+                if tiles[y_index-1][x_index] == TileStatus.UNKNOWN:
+                    tiles[y_index-1][x_index] = TileStatus.OUTSIDE
+                x_index = x_index + 1
+        case "L":
+            match last_direction:
+                case "D":
+                    if x_index < (len(tiles[0])-1):
+                        if tiles[y_index][x_index+1] == TileStatus.UNKNOWN:
+                            tiles[y_index][x_index+1] = TileStatus.OUTSIDE
+                    if y_index < (len(tiles)-1):
+                        if tiles[y_index+1][x_index] == TileStatus.UNKNOWN:
+                            tiles[y_index+1][x_index] = TileStatus.OUTSIDE
+                case "U":
+                    pass
+                case _:
+                    assert(False)
+            target_x_coord = x_coords[x_index] - instr.distance
+            while x_coords[x_index] != target_x_coord:
+                tiles[y_index][x_index] = TileStatus.INSIDE
+                if tiles[y_index+1][x_index] == TileStatus.UNKNOWN:
+                    tiles[y_index+1][x_index] = TileStatus.OUTSIDE
+                x_index = x_index - 1
+        case "D":
+            match last_direction:
+                case "L":
+                    pass
+                case "R":
+                    if tiles[y_index-1][x_index] == TileStatus.UNKNOWN:
+                        tiles[y_index-1][x_index] = TileStatus.OUTSIDE
+                    if x_index < (len(tiles[0])-1):
+                        if tiles[y_index][x_index+1] == TileStatus.UNKNOWN:
+                            tiles[y_index][x_index+1] = TileStatus.OUTSIDE
+                case _:
+                    assert(False)
+            target_y_coord = y_coords[y_index] + instr.distance
+            while y_coords[y_index] != target_y_coord:
+                tiles[y_index][x_index] = TileStatus.INSIDE
+                if x_index < (len(tiles[0])-1):
+                    if tiles[y_index][x_index+1] == TileStatus.UNKNOWN:
+                        tiles[y_index][x_index+1] = TileStatus.OUTSIDE
+                y_index = y_index + 1
+        case "U":
+            match last_direction:
+                case "L":
+                    if tiles[y_index][x_index-1] == TileStatus.UNKNOWN:
+                        tiles[y_index][x_index-1] = TileStatus.OUTSIDE
+                    if y_index < (len(tiles)-1):
+                        if tiles[y_index+1][x_index] == TileStatus.UNKNOWN:
+                            tiles[y_index+1][x_index] = TileStatus.OUTSIDE
+                case "R":
+                    pass
+                case _:
+                    assert(False)
+            target_y_coord = y_coords[y_index] - instr.distance
+            while y_coords[y_index] != target_y_coord:
+                tiles[y_index][x_index] = TileStatus.INSIDE
+                if tiles[y_index][x_index-1] == TileStatus.UNKNOWN:
+                    tiles[y_index][x_index-1] = TileStatus.OUTSIDE
+                y_index = y_index - 1
+        case _:
+            assert(False)
+    last_direction = instr.direction
+
+assert(x_coord==0)
+assert(y_coord==0)
+assert(x_index==0)
+assert(y_index==0)
+
+#################
+def status_to_char(status: TileStatus) -> str:
+    match status:
+        case TileStatus.UNKNOWN:
+            return "?"
+        case TileStatus.INSIDE:
+            return "I"
+        case TileStatus.OUTSIDE:
+            return "O"
+
+def print_tiles():
+    global tiles
+    for row in tiles:
+        print("".join([status_to_char(s) for s in row]))
+
+print_tiles()
+print("------")
+
+dirty = True
+while dirty:
+    dirty = False
+    for x in range(len(tiles[0])):
+        for y in range(len(tiles)):
+            if tiles[y][x] == TileStatus.UNKNOWN:
+                dirty = True
+                neighbor_status = tiles[y][x-1]
+                if neighbor_status == TileStatus.INSIDE or neighbor_status == TileStatus.OUTSIDE:
+                    tiles[y][x] = neighbor_status
+                    neighbor_status = tiles[y-1][x]
+                    if neighbor_status == TileStatus.INSIDE or neighbor_status == TileStatus.OUTSIDE:
+                        tiles[y][x] = neighbor_status
+                if x < len(tiles[0])-1:
+                    neighbor_status = tiles[y][x+1]
+                    if neighbor_status == TileStatus.INSIDE or neighbor_status == TileStatus.OUTSIDE:
+                        tiles[y][x] = neighbor_status
+                if y < len(tiles)-1:
+                    neighbor_status = tiles[y+1][x]
+                    if neighbor_status == TileStatus.INSIDE or neighbor_status == TileStatus.OUTSIDE:
+                        tiles[y][x] = neighbor_status
+
+print_tiles()
+
+def get_tile_size(x_index:int, y_index:int) -> int:
+    width  = x_coords[x_index] - (x_coords[x_index-1] if x_index > 0 else 0)
+    height = y_coords[y_index] - (y_coords[y_index-1] if y_index > 0 else 0)
+    return width * height
+
+sum_inside_tiles = sum([sum([get_tile_size(x_index=x, y_index=y) if tiles[y][x] == TileStatus.INSIDE else 0 for x in range(len(x_coords))]) for y in range(len(y_coords))])
+print(sum_inside_tiles)
+
+#################
+
+"""
 def instruction_to_command(instructions: list[Instruction], index: int) -> int:
     
     match instructions[index].direction:
@@ -100,6 +264,7 @@ def instruction_to_command(instructions: list[Instruction], index: int) -> int:
 
 commands = [instruction_to_command(instructions=instructions, index=index) for index in range(len(instructions))]
 print(commands)
+"""
 
 #############
 """
